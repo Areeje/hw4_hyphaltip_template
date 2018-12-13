d956ba27fb4e025ef30b4bb962ad6ec7c49fbdca
Areeje <43833530+Areeje@users.noreply.github.com>
2018-12-12 18:39:07 -0800

    Update rand_shuffle_seqs.py

--- rand_shuffle_seqs.py
+++ rand_shuffle_seqs.py
@@ -1,3 +1,5 @@
+
+  
 #!/usr/bin/env python3
 # Using python write a tool which generates a random subsampling tool
 # for sequences. Given a FASTA sequence database file, generate a new
@@ -9,15 +11,29 @@
 import gzip
 import random
 import string
+import sys
+from Bio import SeqIO
 
 # here is an example of how to take a list and extract a random set of values
 # you can comment out this code after you understand it
 # and see how you can do this same approach for a file of sequences
-example_list = list(string.ascii_lowercase)
+#example_list = list(string.ascii_lowercase)
 
-fifty_percent = int(0.5 * len(example_list))
-shuffled_list = random.sample(example_list,fifty_percent)
-print(shuffled_list)
 
-for i in shuffled_list:
+
+#file = gzip.open('yeast_RNASeq_1.fa.gz')
+with gzip.open('yeast_RNASeq_1.fa.gz', 'r') as f:
+    file = f.read()
+file_parse = SeqIO.parse("file", "fasta")
+
+percent = int((float(sys.argv[1])) * len(file_parse))
+
+shuffled = random.sample(file_parse, percent)
+print(shuffled)
+
+
+
+for i in shuffled:
   print(i)
+
+
